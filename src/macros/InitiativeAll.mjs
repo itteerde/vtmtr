9bb917874ce1bd35ab98152f971875a1d667b052
David Milman <dave.mil.mail@gmail.com>
2026-03-04 16:03:29 -0500

    issue #6: creating a macro

--- src/macros/InitiativeAll.mjs
+++ src/macros/InitiativeAll.mjs
@@ -1,6 +1,6 @@
 game.combat.combatants.contents.forEach(c => {
-    console.log({ combatant: c, id: c.actorId, actor: game.actors.get(c.actorId) });
-    if (game.actors.get(c.actorId).type === 'vampire') {
+    //console.log({ combatant: c, id: c.actorId, actor: game.actors.get(c.actorId) });
+    if (game.actors.get(c.actorId).type === 'ghoul' || game.actors.get(c.actorId).type === 'hunter' || game.actors.get(c.actorId).type === 'mortal' || game.actors.get(c.actorId).type === 'vampire' || game.actors.get(c.actorId).type === 'werewolf') {
         c.update({ "initiative": game.actors.get(c.actorId).getRollData().attributes.dexterity.value + game.actors.get(c.actorId).getRollData().attributes.wits.value })
     }
     if (game.actors.get(c.actorId).type === 'spc') {
