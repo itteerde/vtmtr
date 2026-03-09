a4d48a37c4a7fb604caaa2ded5d711d76470136b
Erik Itter <erik.itter@gmail.com>
2026-03-09 20:41:32 +0100

    ...

--- src/spcimporter/spc-importer.mjs
+++ src/spcimporter/spc-importer.mjs
@@ -1,5 +1,14 @@
+import { stdin as input, stdout as output } from 'node:process';
 import fs from 'node:fs/promises';
 import { color, consoleColors } from '../lib/colorize.mjs';
+
+for (const a of process.argv) {
+    if (a.toLocaleLowerCase().startsWith("--help")) {
+
+        console.log(color(consoleColors.green, `so glad you found the help, nothing helpful here.`));
+        process.exit();
+    }
+}
 
 const data = await fs.readFile('./src/spcimporter/v-template.json', 'utf-8');
 const character = JSON.parse(data);
