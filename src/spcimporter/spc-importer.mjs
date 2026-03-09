1ca9bdf7136e07cd1485f221104831d1c78bd21f
Erik Itter <erik.itter@gmail.com>
2026-03-09 20:25:26 +0100

    ...

--- src/spcimporter/spc-importer.mjs
+++ src/spcimporter/spc-importer.mjs
@@ -3,8 +3,14 @@ import fs from 'node:fs/promises';
 const data = await fs.readFile('./src/spcimporter/v-template.json', 'utf-8');
 const character = JSON.parse(data);
 
-console.log(character);
+//console.log(character);
 
 const pasted = await fs.readFile('./src/spcimporter/pasted.txt', 'utf-8');
+
+if (pasted.match(/Sire:\s*(.*)/)) {
+    character.system.headers.sire = pasted.match(/Sire:\s*(.*)/)[1];
+} else {
+    console.log(`sire: not found, skipping`);
+}
 
 await fs.writeFile('./src/spcimporter/import.json', JSON.stringify(character));
