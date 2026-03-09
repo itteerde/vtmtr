a7264e85e79f9ffed185cc60e47b6d2cd7904171
Erik Itter <erik.itter@gmail.com>
2026-03-09 19:51:05 +0100

    ...

--- src/spcimporter/spc-importer.mjs
+++ src/spcimporter/spc-importer.mjs
@@ -1,5 +1,6 @@
 import fs from 'node:fs/promises';
 
 const data = await fs.readFile('./src/spcimporter/v-template.json', 'utf-8');
+const template = JSON.parse(data);
 
-console.log(data);
+console.log(template);
