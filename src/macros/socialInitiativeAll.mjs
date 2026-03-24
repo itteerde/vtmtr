e57563311d320393ac8017c9755787a2fb5db340
David Milman <dave.mil.mail@gmail.com>
2026-03-24 12:38:09 -0400

    socialInitiativeAll.mjs

---
+++ src/macros/socialInitiativeAll.mjs
@@ -0,0 +1,9 @@
+game.combat.combatants.contents.forEach(c => {
+    //console.log({ combatant: c, id: c.actorId, actor: game.actors.get(c.actorId) });
+    if (game.actors.get(c.actorId).type === 'ghoul' || game.actors.get(c.actorId).type === 'hunter' || game.actors.get(c.actorId).type === 'mortal' || game.actors.get(c.actorId).type === 'vampire' || game.actors.get(c.actorId).type === 'werewolf') {
+        c.update({ "initiative": game.actors.get(c.actorId).getRollData().skills.etiquette.value + game.actors.get(c.actorId).getRollData().attributes.wits.value })
+    }
+    if (game.actors.get(c.actorId).type === 'spc') {
+        c.update({ "initiative": Math.max(game.actors.get(c.actorId).getRollData().standarddicepools.mental.value, game.actors.get(c.actorId).getRollData().standarddicepools.social.value) })
+    }
+});
