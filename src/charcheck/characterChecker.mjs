a7b5d4b300dd34ddd0191e3833c7b5dc3415cc97
David Milman <dave.mil.mail@gmail.com>
2026-04-01 14:32:48 -0400

    issue #13

---
+++ src/charcheck/characterChecker.mjs
@@ -0,0 +1,24 @@
+//check for one Attribute at 4; three Attributes at 3; four Attributes at 2; one Attribute at 1.
+
+//set Health = Stamina + 3; Willpower = Composure + Resolve.
+
+//check either jackOfAllTrades One Skill at 3; eight Skills at 2; ten Skills at 1 || balanced Three Skills at 3; five Skills at 2; seven Skills at 1 || specialist: One Skill at 4; three Skills at 3; three Skills at 2; three Skills at 1
+
+//check that all Academics, Craft, Performance, and Science Skills have one speciality
+
+//check that there is one more speciality than the number of above skills taken.
+
+// check for two dots in one Discipline and one dot in another Discipline.
+
+//check Predator type (p. 175) and match for: + one of the listed specialties + one dot to a listed Discipline + associated Advantages or Flaws.
+
+//check Alleycat specialty: Intimidation (Stickups) || Brawl (Grappling); + one dot of Celerity || Potence || - 1 dot of Humanit; three dots of criminal Contacts
+//check Bagger specialty: Larceny(Lockpicking) || Streetwise(Black Market); check one dot of Blood Sorcery (Tremere only) || Obfuscate; check Feeding Merit: Iron Gullet(•••); check Enemy Flaw: (••) Either
+
+//check for 7 points on Advantages
+
+// check for 2 points of flaws
+
+// check for blood potency 1
+
+//check final humanity
